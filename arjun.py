403dd4ffb74abe3ffba359ddb0a9f5dc1186fb7f
jatinthakur029 <jatinthakur432156@gmail.com>
2026-07-27 19:50:55 +0530

    built the core nginx parser engine of Arjun

--- arjun.py
+++ arjun.py
@@ -1,15 +1,34 @@
 import pandas as pd
-
+#Opening the access.log file in read mode to read the logs
 with open("sample_logs/access.log","r") as file:
   logs = file.readlines()
-
-
-for index, log in enumerate(logs):
+  
+def data_parser():
+ #Running a loop for scanning each log in the file
+ for index, log in enumerate(logs):
         parts = logs[index].split()
-       
         data = {}
-        data["ip"] = parts[0]
+        # For parsing IPs
+        data['ip'] = parts[0]
+        # For parsing timestamp
+        data['timestamp'] = parts[3][1:] + " " + parts[4][:5]
+        #For parsing Methods
+        data['method'] = parts[5][1:]
+        #For parsing Endpoint
+        data['endpoint'] = parts[6] 
+        #For parsing protocol
+        data['protocol'] = parts[7][:8]
+        #For parsing Statuscode
+        data['status_code'] = parts[8]
+        #For parsing Bytes
+        data['bytes'] = parts[9]
+        # Printing the dictionary
         print(data)
+                                                                                                                  
+       
+#Calling the function
+data_parser()
+          
         
         
        
